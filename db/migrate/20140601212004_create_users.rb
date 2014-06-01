5bcb2073f900c76bf913f0f9f8296052c0dbccca
Jamie McKenzie <jamiemckenzie415@gmail.com>
2014-06-01 15:57:36 -0700

    Adds database model data validations null: false and uniqueness.

--- db/migrate/20140601212004_create_users.rb
+++ db/migrate/20140601212004_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+      t.string :password, null: false
       t.boolean :sms_remind?
       t.boolean :email_remind?
       t.integer :phone
--- db/migrate/20140601213646_create_payees.rb
+++ db/migrate/20140601213646_create_payees.rb
@@ -1,8 +1,8 @@
 class CreatePayees < ActiveRecord::Migration
   def change
     create_table :payees do |t|
-      t.string :name
-      t.string :recurring
+      t.string :name, null: false, unique: true
+      t.string :recurring, null: false
       t.references :user, index: true
 
       t.timestamps
