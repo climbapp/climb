6fd44d5839ea957b1bc8d1f66d3d748ca8b3a0bd
Jamie McKenzie <jamiemckenzie415@gmail.com>
2014-06-01 16:08:36 -0700

    Begins to add validations on User model. Still testing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, :email, :password, :presence => true
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :phone, :presence => true, :if => :sms_remind?
+
+
 end
