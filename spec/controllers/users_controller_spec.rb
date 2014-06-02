a10ac915661e257868918c6d983fa85e642a52b5
Jamie & Jason Lo <pair+jamiemckenzie.lightninglord@devbootcamp.com>
2014-06-01 18:51:31 -0700

    implemented controller tests for users#new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe UsersController do
   describe "index" do
-    before(:each){get :index}
+    before(:each){ get :index }
 
     it "is successful" do
       expect(response).to be_successful
@@ -12,6 +12,19 @@ describe UsersController do
       expect(assigns(:users)).to eq User.all
     end
   end
+
+  describe "new" do
+    before(:each){ get :new }
+    it "is successful" do
+      expect(response).to be_successful
+    end
+
+    it "assigns @user to a new user" do
+      expect(assigns(:user)).to be_a_new User
+    end
+  end
+
+
 
 
 end
