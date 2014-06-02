2f04c95e28f13511e439ddc3572561b5b3742bd2
Jamie McKenzie <jamiemckenzie415@gmail.com>
2014-06-01 17:41:32 -0700

    Configs user of factory-girl-rails and shoulda-matchers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :phone, :presence => true, :if => :sms_remind?
 
 
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+require 'minitest/autorun'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -13,6 +13,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
