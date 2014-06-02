91b1a7eafc303f59c9cd005335b91bf31ba3d480
Jamie & Jason Lo <pair+jamiemckenzie.lightninglord@devbootcamp.com>
2014-06-01 19:04:14 -0700

    wrote pending tests for UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,6 +24,68 @@ describe UsersController do
     end
   end
 
+  describe "create" do
+    it "assigns @user to a new user"
+    context "format html" do
+      context "valid attributes" do
+        it "creates a new user"
+        it "redirects to @user"
+      end
+
+      context "invalid attributes" do
+        it "does not create a new user"
+        it "renders new_user template"
+      end
+    end
+
+    context "format json" do
+      context "valid attributes" do
+        it "creates a new user"
+        it "renders something"
+      end
+
+      context "invalid attributes" do
+        it "does not create a new user"
+        it "renders something"
+      end
+    end
+  end
+
+  describe "update" do
+    context "format html" do
+      context "valid attributes" do
+        it "updates the user's attributes"
+        it "redirects to @user"
+      end
+
+      context "invalid attributes" do
+        it "doesn't update the user's attributes"
+        it "renders the edit user form"
+      end
+    end
+
+    context "format json" do
+      context "valid attributes" do
+        it "updates the user's attributes"
+        it "renders something"
+      end
+
+      context "invalid attributes" do
+        it "doesn't update the user's attributes"
+        it "renders something"
+      end
+    end
+  end
+
+  describe "destroy" do
+    it "destroys the user"
+    context "format html" do
+      it "redirects to users_url"
+    end
+    context "format json" do
+      it "does something i don't understand"
+    end
+  end
 
 
 
