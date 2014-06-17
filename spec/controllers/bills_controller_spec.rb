df817f695236767d31a3721f37a189d67039fd54
Jason Lo <mr.lo.mhs@gmail.com>
2014-06-17 16:50:41 -0700

    implemented controller tests for bills#new

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe BillsController do
   describe '#index' do
-    before(:each){get :index}
+    before(:each){ get :index }
     it "is successful" do
       expect(response).to be_successful
     end
 
     it "assigns @bills to Bill.all" do
       expect(assigns(:bills)).to eq Bill.all
+    end
+  end
+
+  describe '#new' do
+    before(:each){ get :new }
+    it "is successful" do
+      expect(response).to be_successful
+    end
+
+    it "assigns @bill to a new Bill" do
+      expect(assigns(:bill)).to be_a_new Bill
     end
   end
 
