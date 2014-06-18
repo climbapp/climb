a4e031dd1b6c2b7fb5b0006038e01b2c2a9c9bbf
Jason Lo <mr.lo.mhs@gmail.com>
2014-06-17 17:02:43 -0700

    Implemented controller tests for bills#create
      *for valid attributes and invalid attributes
      *created a bill factory

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -23,4 +23,28 @@ describe BillsController do
     end
   end
 
+  describe '#create' do
+    context '#valid attributes' do
+      let(:create_bill){post :create, bill: FactoryGirl.attributes_for(:bill)}
+      it "creates a bill" do
+        expect{create_bill}.to change{Bill.count}.by(1)
+      end
+      it "redirects" do
+        create_bill
+        expect(response).to be_redirect
+      end
+    end
+
+    context '#invalid attributes' do
+      let(:create_invalid_bill){ post :create, bill: {name: "Iron Bank of Braavos"}}
+      it "doesn't create a bill" do
+        expect{create_invalid_bill}.to change{Bill.count}.by(0)
+      end
+      it "renders the new bill template" do
+        create_invalid_bill
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
 end
