cf27c7846a320260dd02b7aefb8980656c263aa0
Jason Lo <mr.lo.mhs@gmail.com>
2014-06-17 17:47:15 -0700

    implemented controller tests for payees#create with valid and invalid attributes

--- spec/controllers/payees_controller_spec.rb
+++ spec/controllers/payees_controller_spec.rb
@@ -23,4 +23,32 @@ describe PayeesController do
       expect(assigns(:payee)).to be_a_new Payee
     end
   end
+
+  describe '#create' do
+    context 'valid attributes' do
+      let(:create_payee){post :create, :user_id => ned_stark.id, payee:
+        FactoryGirl.attributes_for(:payee)}
+      it "creates a payee" do
+        expect{ create_payee }.to change{ Payee.count }.by(1)
+      end
+      it "redirects" do
+        create_payee
+        expect(response).to be_redirect
+      end
+      it "assigns the payee to the correct user" do
+        expect{ create_payee }.to change{ ned_stark.payees.count }.by(1)
+      end
+    end
+    context 'invalid attributes' do
+      let(:create_invalid_payee){post :create, :user_id => ned_stark.id,
+        payee: {name: nil }}
+      it "doesn't create a payee" do
+        expect{ create_invalid_payee }.to change{ Payee.count }.by(0)
+      end
+      it "renders new payee form" do
+        create_invalid_payee
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
