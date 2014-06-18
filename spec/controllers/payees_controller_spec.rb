4913caa0e1e957e8a866d545797887d6823369da
Jason Lo <mr.lo.mhs@gmail.com>
2014-06-17 17:39:33 -0700

    implemented controller tests for payees#new

--- spec/controllers/payees_controller_spec.rb
+++ spec/controllers/payees_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 describe PayeesController do
+  let(:ned_stark){FactoryGirl.create(:user_phone)}
   describe '#index' do
     before(:each){get :index}
     it "is successful" do
@@ -10,5 +11,16 @@ describe PayeesController do
     end
   end
 
-
+  describe '#new' do
+    before(:each){ get :new, :user_id => ned_stark.id }
+    it "is successful" do
+      expect(response).to be_successful
+    end
+    it "assigns @user to the user from params" do
+      expect(assigns(:user)).to eq ned_stark
+    end
+    it "assigns @payee to be a new payee" do
+      expect(assigns(:payee)).to be_a_new Payee
+    end
+  end
 end
