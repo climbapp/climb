ade7036d2564caace1afda78cad589e76dad45da
Jamie McKenzie <jamiemckenzie415@gmail.com>
2014-06-25 01:19:28 -0700

    Final migration of working tests.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601214924) do
+ActiveRecord::Schema.define(version: 20140625044434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20140601214924) do
   end
 
   add_index "bills", ["payee_id"], name: "index_bills_on_payee_id", using: :btree
+
+  create_table "password_resets", force: true do |t|
+    t.integer  "user_id"
+    t.string   "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "password_resets", ["user_id"], name: "index_password_resets_on_user_id", using: :btree
 
   create_table "payees", force: true do |t|
     t.string   "name",       null: false
